074a8f0ca43a3bfa2132263a109069fa5763b29f
natalia <dnataliacr@gmail.com>
2022-07-07 13:59:21 -0300

    create ticket item component

---
+++ client/src/components/Ticket.js
@@ -0,0 +1,21 @@
+import Status from './Status'
+import { BsChevronRight } from 'react-icons/bs'
+function Ticket({ subject, date, from, status, body }) {
+    return (
+        <div className="ticketBox">
+            <div className='ticketHeader'> <span> De:{' ' + from}</span><span> Enviado: {' ' + date}</span>
+            </div>
+            <div className='ticketBody'>
+                <Status status={status} /> <strong>{subject}</strong>
+                <span>{body}</span>
+            </div>
+            <div className='breadCrumbBox'>
+                <span className='breadCrumbIcon'> <BsChevronRight/> </span>
+            </div> 
+
+
+        </div>
+    );
+}
+
+export default Ticket;
